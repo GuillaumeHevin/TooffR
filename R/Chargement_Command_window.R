7cd770ba754d073c83174cf4b988a83234cc67ff
Guillaume Hévin <guillaume.hevin@inrae.fr>
2024-07-25 18:12:23 +0200

    adding documentation

--- R/Chargement_Command_window.R
+++ R/Chargement_Command_window.R
@@ -27,7 +27,6 @@
 #' @param size Number of total elements in the loop
 #' @param text Text to add before the progress bar (default = '')
 #' @param minimal if FALSE, a progress bar will appear, if TRUE only the percentage is shown
-#' @export
 #' 
 #' @examples
 #'  
@@ -35,18 +34,19 @@
 #' Chargement_Command_window(100)
 #' for(i in 1:100)
 #'  {
-#'    Chargement_Command_window()
 #'    Sys.sleep(0.15)
+#'    Chargement_Command_window()
 #'  }
 #' 
-#' #Use in double loops without progress bar with a descriptive text
+#' 
+#' #Use in double loops without progress bar (just percentage) with a descriptive text
 #' Chargement_Command_window(6*7, text = 'Waiting...', minimal = T)
 #' for (j in 1:6)
 #' {
 #'  for(i in 1:7)
 #'  {
-#'    Chargement_Command_window()
 #'    Sys.sleep(0.15)
+#'    Chargement_Command_window()
 #'  }
 #' }
 
@@ -56,38 +56,42 @@ Chargement_Command_window <- function(size, text = '', minimal = FALSE) {
   
   if (nargs() > 0)
   {
+    #Initialisation of the progress bar----
     if (nchar(text) > 30)
     {
       text <- sprintf(' %-30s',paste0(substr(text,1,10),'...',substr(text,nchar(text)-16,nchar(text))))
     }else if (!text == ''){
       text <- sprintf(' %-30s',text)
     }
+    ##Initialisation of global variables----
     CCW_I <<- 0
     CCW_TXT <<- text
     CCW_NBRE <<- size
     CCW_MINIMAL <<- minimal
     
-    #Initialize print
-    percent <- (CCW_I / CCW_NBRE) * 100
-    
+    ##Initialize print----
+    percent <- 0
     if (!CCW_MINIMAL){
-      if (percent == 100){fleche <- '='}else{fleche <- '>'}
       Nb_trait <- max(c(percent %/% interval_trait -1,0))
-      empty <- 100/interval_trait - Nb_trait -1
-      bar <- paste0(' [',strrep('=', Nb_trait),fleche,strrep(' ', empty),']')
+      empty <- 100/interval_trait - Nb_trait
+      bar <- paste0(' [',strrep('=', Nb_trait),strrep(' ', empty),']')
     }else{
       bar <- ''
     }
-    
     cat(paste0("\r",CCW_TXT,bar, sprintf(' %3s%%',sprintf('%.0f',percent))))
     
+    
   }else{
+    #In the Loop: updating the progress bar----
     if (!exists('CCW_I'))
     {
       stop('Chargement_Command_window needs initialisation or size argument is to small compared to the loop')
     }
+    
+    ##Increment the progress bar----
     CCW_I <<- CCW_I +1
     
+    ##update progress bar----
     percent <- (CCW_I / CCW_NBRE) * 100
     
     if (!CCW_MINIMAL){
@@ -101,12 +105,38 @@ Chargement_Command_window <- function(size, text = '', minimal = FALSE) {
     
     cat(paste0("\r",CCW_TXT,bar, sprintf(' %3s%%',sprintf('%.0f',percent))))
     
+    #End of the loop, clear all global variables----
     if (percent == 100){
       rm(CCW_I,CCW_TXT,CCW_NBRE,CCW_MINIMAL, envir = .GlobalEnv)
       cat('\n')
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
